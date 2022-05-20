dde31c3499ca41ae74245a116b5988dae1cd6585
Gleydson Freitas <100448127+GleydsonFB@users.noreply.github.com>
2022-05-20 17:28:19 -0300

    Terceiro commit
    Refinada as funções de mostrar e adicionar itens no inventário.

--- The good RPG/Utilitarios/Inventario.py
+++ The good RPG/Utilitarios/Inventario.py
@@ -3,17 +3,30 @@ class inventario:
         self.tamanho = tamanho
         self.bag = []
         for espaco in range(self.tamanho):
-            self.bag.append(f'{espaco+1}° ------ Vazio')
+            self.bag.append('Vazio')
 
 
     def mostrar(self):
-        print(self.bag)
+        for espacos in range(self.tamanho):
+            tratamento = self.bag[espacos]
+            tratamento.replace("'", "")
+            tratamento.replace('[', '')
+            tratamento.replace(']', '')
+            self.bag[espacos] = tratamento
+        for visual in range(self.tamanho):
+            print(f'{visual +1}° -- {self.bag[visual]}')
 
     def adicionar(self, item):
         verificador = 0
+        adicionado = False
         while verificador < self.tamanho:
             if 'Vazio' not in self.bag[verificador]:
                 verificador += 1
             else:
                 self.bag[verificador] = item
+                adicionado = True
                 break
+        if adicionado is False:
+            print('Bag cheia')
+        else:
+            print(f'Item {item} adicionado com sucesso na slot {verificador+1}')

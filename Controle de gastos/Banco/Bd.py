f8aa27f0f189be4360df0d16d63b6ab0b36a5635
Gleydson Freitas <100448127+GleydsonFB@users.noreply.github.com>
2022-07-26 21:36:56 -0300

    Terceiro commit
    Refatorado parte dos métodos de Crud;
    Incluído novas opções para manipular categorias e valores.

--- Controle de gastos/Banco/Bd.py
+++ Controle de gastos/Banco/Bd.py
@@ -8,6 +8,7 @@ class Conector:
         self.pasw = pword
         self.bd = bd
         self.conexao = mysql.connector.connect(host=self.host, database=self.bd, user=self.user, password=self.pasw)
+        self.cursor = self.conexao.cursor()
 
     def conectar(self):
         return self.conexao
@@ -19,34 +20,11 @@ class Conector:
         else:
             print('Desconectado do banco')
 
-    def adicionar_valor(self, valor, mes, total_compra, categoria):
-        if self.conexao.is_connected():
-            sql = 'INSERT INTO valor (registro, mes, compra_total, categoria) VALUES ({}, {}, {}, {})' \
-                .format(valor, mes, total_compra, categoria)
-            cursor = self.conexao.cursor()
-            cursor.execute(sql)
-            self.conexao.commit()
-            print('Valor inserido com sucesso!')
-        else:
-            print('Sem conexão com o servidor')
-
-    def adicionar_compra_p(self, total_compra, parcelas):
-        if self.conexao.is_connected():
-            sql = 'INSERT INTO total_compra (compra, t_parcela) VALUES ("{}", "{}")' \
-                .format(total_compra, parcelas)
-            cursor = self.conexao.cursor()
-            cursor.execute(sql)
-            self.conexao.commit()
-            print('Valor inserido com sucesso!')
-        else:
-            print('Sem conexão com o servidor')
-
     def select_simples(self, coluna1, coluna2, tabela):
         if self.conexao.is_connected():
             sql = f"SELECT {coluna1}, {coluna2} FROM {tabela};"
-            cursor = self.conexao.cursor()
-            cursor.execute(sql)
-            for c1, c2 in cursor:
+            self.cursor.execute(sql)
+            for c1, c2 in self.cursor:
                 return c1, c2
         else:
             print('Sem conexão com o servidor')
@@ -56,60 +34,155 @@ class Conector:
             match total_colunas:
                 case 1:
                     sql = f"SELECT {coluna1} FROM {tabela} WHERE {colunap} = {pesquisa}"
-                    cursor = self.conexao.cursor()
-                    cursor.execute(sql)
-                    for c1 in cursor:
+                    self.cursor.execute(sql)
+                    for c1 in self.cursor:
                         return c1
                 case 2:
                     sql = f"SELECT {coluna1}, {coluna2} FROM {tabela} WHERE {colunap} = {pesquisa};"
-                    cursor = self.conexao.cursor()
-                    cursor.execute(sql)
-                    for c1, c2 in cursor:
+                    self.cursor.execute(sql)
+                    for c1, c2 in self.cursor:
                         return c1, c2
                 case 3:
                     sql = \
                         f"SELECT {coluna1}, {coluna2}, {demais_colunas[0]} FROM {tabela} WHERE {colunap} = {pesquisa};"
-                    cursor = self.conexao.cursor()
-                    cursor.execute(sql)
-                    for c1, c2, c3 in cursor:
+                    self.cursor.execute(sql)
+                    for c1, c2, c3 in self.cursor:
                         return c1, c2, c3
                 case 4:
                     sql = \
                         f"SELECT {coluna1}, {coluna2}, {demais_colunas[0]}, {demais_colunas[1]} FROM {tabela} WHERE {colunap} = {pesquisa};"
-                    cursor = self.conexao.cursor()
-                    cursor.execute(sql)
-                    for c1, c2, c3, c4 in cursor:
+                    self.cursor.execute(sql)
+                    for c1, c2, c3, c4 in self.cursor:
                         return c1, c2, c3, c4
                 case 5:
                     sql = \
                         f"SELECT * FROM {tabela} WHERE {colunap} = {pesquisa};"
-                    cursor = self.conexao.cursor()
-                    cursor.execute(sql)
-                    for c1, c2, c3, c4, c5 in cursor:
+                    self.cursor.execute(sql)
+                    for c1, c2, c3, c4, c5 in self.cursor:
                         return c1, c2, c3, c4, c5
         else:
             print('Sem conexão com servidor')
 
-    def somar_gasto(self, mes, categoria='nenhuma'):
+    def somar_gasto(self, mes):
         if self.conexao.is_connected():
-            if categoria == 'nenhuma':
-                sql = f"SELECT SUM(registro) FROM valor WHERE mes = {mes};"
-                cursor = self.conexao.cursor()
-                cursor.execute(sql)
-                for c1 in cursor:
-                    return c1
+            sql = f"SELECT SUM(registro) FROM valor WHERE mes = {mes};"
+            self.cursor.execute(sql)
+            for c1 in self.cursor:
+                return c1
+        else:
+            print('Sem conexão com servidor')
 
 
 class Categoria:
     def __init__(self, conexao):
         self.conexao = conexao
+        self.cursor = self.conexao.cursor()
 
     def inserir_categoria(self, nome, limite, minimo=0):
         if self.conexao.is_connected():
-            sql = 'INSERT INTO categoria (nome, limite_gasto, minimo_gasto) VALUES ("{}", "{}", "{}")'.format(nome, limite, minimo)
-            cursor = self.conexao.cursor()
-            cursor.execute(sql)
-            self.conexao.commit()
-            print(f"Categoria {nome} adicionada com sucesso!")
+            if minimo != 0:
+                sql = 'INSERT INTO categoria (nome, limite_gasto, minimo_gasto) VALUES ("{}", "{}", "{}")'.format(nome, limite, minimo)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f"Categoria {nome} adicionada com sucesso!")
+            else:
+                sql = 'INSERT INTO categoria (nome, limite_gasto) VALUES ("{}", "{}")'.format(nome, limite)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f"Categoria {nome} adicionada com sucesso!")
         else:
             print('Sem conexão com o servidor')
+
+    def alterar_categoria(self, id_cat, nome='', limite='', minimo=''):
+        if self.conexao.is_connected():
+            if nome != '' and limite == '' and minimo == '':
+                sql = 'UPDATE categoria SET nome = "{}" WHERE id_cat = "{}"'.format(nome, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Novo nome da categoria definido como {nome}!')
+            elif nome != '' and limite != '' and minimo == '':
+                sql = 'UPDATE categoria SET nome = "{}", limite_gasto = "{}" WHERE id_cat = "{}"'.format(nome, limite, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Alterado nome e limite para: {nome} e {limite}!')
+            elif nome != '' and limite != '' and minimo != '':
+                sql = 'UPDATE categoria SET nome = "{}", limite_gasto = "{}", minimo_gasto = "{}" WHERE id_cat = "{}"'.format(nome, limite, minimo, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Alterado nome, limite e minimo gasto para: {nome}, R${limite} e R${minimo}!')
+            elif nome == '' and limite != '' and minimo == '':
+                sql = 'UPDATE categoria SET limite_gasto = "{}" WHERE id_cat = "{}"'.format(limite, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Novo limite definido para R${limite}!')
+            elif nome == '' and limite != '' and minimo != '':
+                sql = 'UPDATE categoria SET limite_gasto = "{}", minimo_gasto = "{}" WHERE id_cat = "{}"'.format(limite, minimo, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Novo limite definido para R${limite} e mínimo de R${minimo}!')
+            elif nome == '' and limite == '' and minimo != '':
+                sql = 'UPDATE categoria SET minimo_gasto = "{}" WHERE id_cat = "{}"'.format(minimo, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Novo valor mínimo definido para R${minimo}!')
+            elif nome != '' and limite == '' and minimo != '':
+                sql = 'UPDATE categoria SET nome = "{}", minimo_gasto = "{}" WHERE id_cat = "{}"'.format(nome, minimo, id_cat)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print(f'Nome alterado para {nome} e com novo valor mínimo de R${minimo}!')
+            else:
+                print('Opção inválida')
+
+    #def deletar_categoria(self):
+
+    def somar_gasto_cat(self, mes, categoria):
+        if self.conexao.is_connected():
+            sql = f"SELECT SUM({categoria}) FROM valor WHERE mes = {mes};"
+            self.cursor.execute(sql)
+            for c1 in self.cursor:
+                return c1
+
+
+class Compra:
+    def __init__(self, conexao):
+        self.conexao = conexao
+        self.cursor = self.conexao.cursor()
+
+    def adicionar_valor(self, valor, mes, categoria, total_compra=''):
+        if self.conexao.is_connected():
+            if total_compra != '':
+                sql = 'INSERT INTO valor (registro, mes, compra_total, categoria) VALUES ({}, {}, {}, {})' \
+                    .format(valor, mes, total_compra, categoria)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print('Valor inserido com sucesso!')
+            else:
+                sql = 'INSERT INTO valor (registro, mes, categoria) VALUES ({}, {}, {})' \
+                    .format(valor, mes, categoria)
+                self.cursor.execute(sql)
+                self.conexao.commit()
+                print('Valor inserido com sucesso!')
+        else:
+            print('Sem conexão com o servidor')
+
+    def adicionar_compra_p(self, total_compra, parcelas):
+        if self.conexao.is_connected():
+            sql = 'INSERT INTO total_compra (compra, t_parcela) VALUES ("{}", "{}")' \
+                .format(total_compra, parcelas)
+            self.cursor.execute(sql)
+            self.conexao.commit()
+            print('Valor inserido com sucesso!')
+        else:
+            print('Sem conexão com o servidor')
+
+    def remover_compra_p(self, id_compra):
+        if self.conexao.is_connected():
+            sql = f'DELETE FROM valor WHERE compra_total = {id_compra}'
+            sql2 = f'DELETE FROM total_compra WHERE id_compra = {id_compra}'
+            self.cursor.execute(sql)
+            self.conexao.commit()
+            self.cursor.execute(sql2)
+            self.conexao.commit()
+            print('Compra retirada com sucesso')
+        else:
+            print('Erro no servidor')
--- Controle de gastos/bin.py
+++ Controle de gastos/bin.py
@@ -2,3 +2,10 @@ from Banco import Bd
 
 bd = Bd.Conector('localhost', 'root', 'root', 'controle_de_gastos')
 conexao = bd.conectar()
+categorias = Bd.Categoria(conexao)
+#categorias.inserir_categoria('Jogos', 500)
+compra1 = Bd.Compra(conexao)
+#compra1.adicionar_compra_p(900, 9)
+#compra1.adicionar_valor(90, 3, 1, 1)
+#a = bd.select_composto(4, 'valor', 'registro', 'compra_total', 'id_valor', 2, 'mes', 'categoria')
+categorias.alterar_categoria(1, limite=20)
